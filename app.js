260c029f5d686e78f83ac8cb6aa34212e53beb9b
marcio-cavalcante <marcio_acc@hotmail.com>
2024-09-08 09:31:03 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,16 +1,31 @@
 function pesquisar() {
 
-    // Obtém a seção HTML onde os resultados da pesquisa serão exibidos
-    let section = document.getElementById("resultados-pesquisa");
-    console.log(section); // Linha para depuração, mostra o elemento no console
-  
-    // Inicializa uma string vazia para armazenar os resultados da pesquisa
-    let resultados = "";
-  
-    // Itera sobre cada elemento (dado) no array 'dados'
-    for (let dado of dados) {
-      // Concatena HTML para cada resultado, formatando os dados do objeto
-      resultados += `
+  // Obtém a seção HTML onde os resultados da pesquisa serão exibidos
+  let section = document.getElementById("resultados-pesquisa");
+
+  let campoPesquisa = document.getElementById("campo-pesquisa").value;
+
+  if (!campoPesquisa) {
+    section.innerHTML = "<p>Digite algo!</p>"
+    return
+  };
+
+  campoPesquisa = campoPesquisa.toLowerCase()
+
+  // Inicializa uma string vazia para armazenar os resultados da pesquisa
+  let resultados = "";
+  let titulo = "";
+  let descricao = "";
+  let tags = "";
+
+  // Itera sobre cada elemento (dado) no array 'dados'
+  for (let dado of dados) {
+    titulo = dado.titulo.toLowerCase()
+    descricao = dado.descricao.toLowerCase()
+    tags = dado.tags.toLocaleLowerCase()
+    if (titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa) || tags.includes(campoPesquisa)) {
+    // Concatena HTML para cada resultado, formatando os dados do objeto
+    resultados += `
         <div class="item-resultado">
           <h2>
             <a href="https://pt.wikipedia.org/wiki/Rebeca_Andrade" target="_blank">${dado.titulo}</a>
@@ -18,9 +33,14 @@ function pesquisar() {
           <p class="descricao-meta">${dado.descricao}</p>
           <a href=${dado.link} target="_blank">Mais info.</a>
         </div>
-      `;
+      `;      
     }
-  
-    // Atribui a string com os resultados ao conteúdo HTML da seção
-    section.innerHTML = resultados;
   }
+
+  if (!resultados) {
+    resultados = "<p>Não existe</p>"
+  }
+
+  // Atribui a string com os resultados ao conteúdo HTML da seção
+  section.innerHTML = resultados;
+}
--- dados.js
+++ dados.js
@@ -2,21 +2,19 @@ let dados = [
     {
       titulo: "Rayssa Leal",
       descricao: "Rayssa Leal: A fadinha do skate brasileiro. Rayssa Leal é uma skatista brasileira que conquistou o mundo com seu talento e carisma, sendo a mais jovem medalhista olímpica da história do Brasil.",
-      link: "https://pt.wikipedia.org/wiki/Rayssa_Leal"
+      link: "https://pt.wikipedia.org/wiki/Rayssa_Leal",
+      tags: "skate fada sk8"
     },
     {
       titulo: "Beatriz Souza",
       descricao: "Beatriz Souza: A nova estrela do judô brasileiro. Beatriz Souza é uma judoca brasileira que vem se destacando no cenário internacional, com diversas medalhas em competições importantes.",
-      link: "https://pt.wikipedia.org/wiki/Beatriz_Souza"
+      link: "https://pt.wikipedia.org/wiki/Beatriz_Souza",
+      tags: "judo judô judoca judoka"
     },
     {
       titulo: "Rebeca Andrade",
       descricao: "Rebeca Andrade: A estrela da ginástica brasileira. Rebeca Andrade é uma ginasta artística brasileira que conquistou o coração dos brasileiros e se tornou um dos maiores nomes do esporte nacional.",
-      link: "https://pt.wikipedia.org/wiki.org/wiki/Rebeca_Andrade"
-    },
-    {
-      titulo: "Tatiana Weston-Webb",
-      descricao: "Tatiana Guimarães Weston-Webb (Porto Alegre, 9 de maio de 1996) é uma surfista profissional brasileira-americana, radicado em Kauai, Havaí, que está na World Women's Championship Tour",
-      link: "https://pt.wikipedia.org/wiki/Tatiana_Weston-Webb"
+      link: "https://pt.wikipedia.org/wiki.org/wiki/Rebeca_Andrade",
+      tags: "ginástica ginastica ouro"
     }
   ];
